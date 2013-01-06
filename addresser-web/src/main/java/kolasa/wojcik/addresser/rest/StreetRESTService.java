04a3c5907217aa0cca7721895846d47cf6fcf7aa
fatfredyy <fatfredyy@fatfredyy.net>
2013-01-06 17:14:52 +0100

    Extracted rest api.

--- addresser-web/src/main/java/kolasa/wojcik/addresser/rest/StreetRESTService.java
+++ addresser-web/src/main/java/kolasa/wojcik/addresser/rest/StreetRESTService.java
@@ -2,9 +2,6 @@ package kolasa.wojcik.addresser.rest;
 
 import java.util.List;
 
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -12,60 +9,26 @@ import javax.ws.rs.Produces;
 
 import kolasa.wojcik.addresser.model.Street;
 
-import com.google.common.base.Strings;
-
 @Path("/streets")
-@RequestScoped
-public class StreetRESTService {
-   @Inject
-   private EntityManager em;
+public interface StreetRESTService {
 
-   @GET
-   @Produces("text/xml")
-   public List<Street> listAllStreets() {
-      final List<Street> results = em.createQuery("select s from Street s", Street.class).getResultList();
-      return results;
-   }
+	@GET
+	@Produces("text/xml")
+	public List<Street> listAllStreets();
 
-   @GET
-   @Path("/{id:[0-9][0-9]*}")
-   @Produces("text/xml")
-   public Street lookupMemberById(@PathParam("id") long id) {
-      return em.find(Street.class, id);
-   }
-   
-   
-   
-   @GET
-   @Path("/desc/{desc}")
-   @Produces("text/xml")
-   public Street lookupMemberById(@PathParam("desc") String desc) {
-	   Street street = em
-				.createQuery(
-						"select s from Street s where lower(s.description) = :description",
-						Street.class).setParameter("description", desc)
-				.getSingleResult();
-		return street;
-   }
-   @GET
-   @Path("/suggest/{suggest}")
-   @Produces("text/xml")
-   public List<Street> suggestStreets(@PathParam("suggest")String pattern) {
-		List<Street> matchingStreets = em
-				.createQuery(
-						"select s from Street s where lower(s.description) like :description",
-						Street.class)
-				.setParameter("description", getSearchPattern(pattern))
-				.getResultList();
-		
-		return matchingStreets;
-	}
+	@GET
+	@Path("/{id:[0-9][0-9]*}")
+	@Produces("text/xml")
+	public Street lookupMemberById(@PathParam("id") long id);
 
-	private String getSearchPattern(String pattern) {
-		if (Strings.isNullOrEmpty(pattern)) {
-			return "%";
-		} else {
-			return "%" + pattern.toLowerCase().replace('*', '%') + "%";
-		}
-	}
+	@GET
+	@Path("/desc/{desc}")
+	@Produces("text/xml")
+	public Street lookupMemberById(@PathParam("desc") String desc);
+
+	@GET
+	@Path("/suggest/{suggest}")
+	@Produces("text/xml")
+	public List<Street> suggestStreets(@PathParam("suggest") String pattern);
+
 }
---
+++ addresser-web/src/main/java/kolasa/wojcik/addresser/rest/StreetRESTServiceImpl.java
@@ -0,0 +1,56 @@
+package kolasa.wojcik.addresser.rest;
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import kolasa.wojcik.addresser.model.Street;
+
+import com.google.common.base.Strings;
+
+@RequestScoped
+public class StreetRESTServiceImpl implements StreetRESTService {
+	
+	@Inject
+	private EntityManager em;
+
+	public List<Street> listAllStreets() {
+		final List<Street> results = em.createQuery("select s from Street s",
+				Street.class).getResultList();
+		return results;
+	}
+
+	public Street lookupMemberById(long id) {
+		return em.find(Street.class, id);
+	}
+
+	public Street lookupMemberById(String desc) {
+		Street street = em
+				.createQuery(
+						"select s from Street s where lower(s.description) = :description",
+						Street.class).setParameter("description", desc)
+				.getSingleResult();
+		return street;
+	}
+
+	public List<Street> suggestStreets(String pattern) {
+		List<Street> matchingStreets = em
+				.createQuery(
+						"select s from Street s where lower(s.description) like :description",
+						Street.class)
+				.setParameter("description", getSearchPattern(pattern))
+				.getResultList();
+
+		return matchingStreets;
+	}
+
+	private String getSearchPattern(String pattern) {
+		if (Strings.isNullOrEmpty(pattern)) {
+			return "%";
+		} else {
+			return "%" + pattern.toLowerCase().replace('*', '%') + "%";
+		}
+	}
+}
